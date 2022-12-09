ddb86921d3b48bf762873ba17c57d6bcc524ace7
Hessel Bosma <hessel.bosma@saabgroup.com>
2022-12-09 14:39:48 +0100

    bot van Hessel

--- Hessel.java
+++ Hessel.java
@@ -14,17 +14,20 @@ public class Hessel extends Robot
 	 */
 	public void run() {
 		// Initialization of the robot should be put here
+		
+		turnGunRight(90);
 
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
 		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
 
+			
+
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
 			ahead(3000000);
-			turnGunLeft(360);
 		}
 	}
 
@@ -49,7 +52,6 @@ public class Hessel extends Robot
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
 		turnRight(90);
 	}	
 }
