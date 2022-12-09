6ff679d4fd07d53ecd48932f2295c2a642ba8976
Ronald Leppink <rle@masevon.com>
2022-12-09 11:57:59 +0100

    Hardcode paths, remove class files

--- compile_all_bots.sh
+++ compile_all_bots.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-for file in ./*.java; do
-	javac -encoding UTF-8 -classpath ../../libs/robocode.jar:/Users/Guest/Robocode/robots $file
+rm -f /Users/Guest/Robocode/robots/engineero/*.class
+
+for file in /Users/Guest/Robocode/robots/engineero/*.java; do
+	javac -encoding UTF-8 -classpath /Users/Guest/Robocode/libs/robocode.jar:/Users/Guest/Robocode/robots $file
 done
