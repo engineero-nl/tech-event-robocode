a2007dec1ca881a0fdb581a7184ff0f0fcd35eeb
SE20 <sharon.engbers@engineero.nl>
2022-12-09 16:39:26 +0100

    New robot sharon

--- Sharon.java
+++ Sharon.java
@@ -1,75 +1,79 @@
 package engineero;
 import robocode.*;
 import java.awt.Color;
-
+import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
-import robocode.HitRobotEvent;
-import robocode.Robot;
 import robocode.ScannedRobotEvent;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-
-import java.awt.*;
-
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+import robocode.util.Utils;
 
 /**
  * Sharon - a robot by Sharon
  */
-public class Sharon extends Robot
+public class Sharon extends AdvancedRobot
 {
-	int dist = 50;
-	
-   	public void run(){
-		// Set colors
-		setBodyColor(Color.orange);
-		setGunColor(Color.pink);
-		setRadarColor(Color.red);
-		setScanColor(Color.red);
-		setBulletColor(Color.yellow);
-      
-		while(true){
+	//Global variables
+	static double direction;
+	static double enemyEnergy;
+	static double hits;
+	static int movementMode;
+
+	public void run() {
+		setTurnRadarRightRadians(direction = 100000);
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		//Local variables
+		double absoluteBearing;
+		double bearing;
+		double lead;	
 		
-         	ahead(100);
-		    turnGunRight(360);
-		    back(75);
-		    turnGunRight(360);
-      	}
-   	}
-	
-	public void onScannedRobot(ScannedRobotEvent e){
-		if (e.getDistance() < 50 && getEnergy() > 50) {
-			fire(3);
+		//gun
+		setTurnGunRightRadians(Utils.normalRelativeAngle((absoluteBearing = getHeadingRadians() + (bearing = e.getBearingRadians())) +
+			(lead = Math.asin(e.getVelocity() / 14 * Math.sin(e.getHeadingRadians() - absoluteBearing))) - getGunHeadingRadians()));
+			
+		//movement		
+		//oscillating			
+		if(enemyEnergy > (enemyEnergy = e.getEnergy()) && movementMode == 1) {
+			onHitWall(null);
 		}
-		else {
-			fire(1);
+		//orbiting
+		setAhead(direction);
+		setTurnRightRadians(Math.cos(bearing - ((absoluteBearing = e.getDistance()) - 160) * (direction / 35000000)));
+		
+		//ramming
+		if (movementMode > 1) {
+			setTurnRightRadians(Math.tan(bearing += lead));
+			setAhead(Math.cos(bearing) / 0);
 		}
+		
+		//gun
+		setFire(Math.min(2 + (100 / (int)absoluteBearing), enemyEnergy / 4));
 	
-		scan();
+		//radar
+		setTurnRadarLeftRadians(getRadarTurnRemaining());
+	}
+
+	public void onBulletHit (BulletHitEvent e) {
+		enemyEnergy -= 10;
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+		if ((hits += (0.4)) > getRoundNum() + 2) {
+			movementMode++;
+			hits = 0;
+		}
 	}
 	
 	public void onHitRobot(HitRobotEvent e) {
-		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
-		
-		turnGunRight(turnGunAmt);
-		fire(3);
-	}
+			double turnGunAmt = robocode.util.Utils.normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+			
+			turnGunRight(turnGunAmt);
+			fire(3);
+		}
 	
-	public void onHitByBulled(HitByBulletEvent e) {
-		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
-
-		ahead(dist);
-		dist *= -1;
-		scan();
-	}
-  
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
 		double bearing = e.getBearing();
 		turnRight(-bearing); 
 		ahead(100);
-	}	
+	}
 }
